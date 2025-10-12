9e7b06a2dcdb867c652a3a87fcec4e4e33b2af1f
KaitoKobayashi-robot <kaito2003kobayashi@gmail.com>
2025-10-12 13:35:04 +0900

    ⚡️ 画像最適化を使用

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+// @ts-check
+/** @type {import ('next').NextConfig} */
+
 const nextConfig: NextConfig = {
   images: {
+    loader: "custom",
+    loaderFile: "./loader.js",
     remotePatterns: [
       {
         protocol: "https",
@@ -10,6 +15,15 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/_fah/image/:path*",
+        destination:
+          "https://us-central1-nana-project-firebase.cloudfunctions.net/ext-image-processing-api-handler/:path*",
+      },
+    ];
   },
 };
 
