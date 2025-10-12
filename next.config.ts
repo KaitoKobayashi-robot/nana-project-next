026bdb616040f644fb2bd0d65943affe6aba20dc
KaitoKobayashi-robot <kaito2003kobayashi@gmail.com>
2025-10-12 21:32:34 +0900

    ⚡️ svgだけ別で扱う

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,44 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack(config) {
+    // 1. Next.jsにデフォルトで設定されているSVGのルールを取得
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.(".svg"),
+    );
+
+    config.module.rules.push(
+      // 2. 取得したルールを、ReactコンポーネントとしてインポートされるSVGを
+      //    除外するように変更する
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url でインポートされた場合のみ適用
+      },
+      // 3. ReactコンポーネントとしてインポートされるSVG用の新しいルールを追加
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // *.svg?url 以外でインポートされた場合に適用
+        use: ["@svgr/webpack"],
+      },
+    );
+
+    // 4. もとのSVGルールを無効化
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
   async rewrites() {
     return [
       {
