dc5187ce0eb18e64dad0495b39425fc01a2f59d7
KaitoKobayashi-robot <kaito2003kobayashi@gmail.com>
2025-10-18 17:18:48 +0900

    🔥

--- next.config.ts
+++ next.config.ts
@@ -45,25 +45,14 @@ const nextConfig: NextConfig = {
 
     return config;
   },
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
       },
-    },
+    }
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/_fah/image/:path*",
-  //       destination:
-  //         "https://us-central1-nana-project-firebase.cloudfunctions.net/ext-image-processing-api-handler/:path*",
-  //     },
-  //   ];
-  // },
 };
 
 export default nextConfig;
