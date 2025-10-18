4e2616fdf14095fdf3a1489836fe15027ec30fb8
KaitoKobayashi-robot <kaito2003kobayashi@gmail.com>
2025-10-18 17:18:36 +0900

    💄 iPhone SE Gen 2 側ではボタンが二つ、PC側では一つに変更

--- src/app/_components/Navigation.tsx
+++ src/app/_components/Navigation.tsx
@@ -3,19 +3,32 @@ import Link from "next/link";
 export const NavButtons = () => {
   const buttonStyle =
     "hover:opacity-50 w-60 h-20 flex justify-center border-2 box-border border-[#2c2522] items-center cursor-pointer rounded-md px-4 py-2 text-center font-bold shadow-xl transition-colors duration-200 text-2xl";
-
+  const liquidGlassStyle =
+    " text-white p-10 active:bg-[#5fc5be]/50 active:scale-110 active:rounded-4xl hover:scale-105 hover:rounded-3xl hover:border-b-3 hover:bg-[#5fc5be]/35 transition-all duration-300 ease-[cubic-bezier(0.34,1.56,0.64,1)] container mx-auto rounded-2xl bg-[#2c2522]/10 backdrop-blur-[4px] p-6 border-b-2 border-white/30 shadow-lg";
   return (
-    <div className="flex flex-col space-y-6">
+    <div className="flex w-[18rem] flex-col space-y-8">
       <Link href="/camera">
-        <div className={`${buttonStyle} `}>本番</div>
+        <div
+          className={`${liquidGlassStyle} flex justify-center text-2xl font-bold lg:hidden`}
+        >
+          本番
+        </div>
       </Link>
 
       <Link href="/camera/adjustment">
-        <div className={`${buttonStyle} `}>カメラ調節</div>
+        <div
+          className={`${liquidGlassStyle} flex justify-center text-2xl font-bold lg:hidden`}
+        >
+          カメラ調節
+        </div>
       </Link>
 
       <Link href="/gallery">
-        <div className={`${buttonStyle} `}>ギャラリー</div>
+        <div
+          className={`${liquidGlassStyle} hidden justify-center text-2xl font-bold lg:flex`}
+        >
+          ギャラリー
+        </div>
       </Link>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,11 @@ import Logo from "../../public/logo.svg";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <main>
-        <Logo
-          width={150}
-          height={150}
-          className="absolute top-0 left-1/2 mt-20 -translate-x-1/2"
-        />
-        <div className="mt-30">
-          <NavButtons />
-        </div>
-      </main>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <Logo width={150} height={150} />
+      <div className="mt-10">
+        <NavButtons />
+      </div>
+    </main>
   );
 }
