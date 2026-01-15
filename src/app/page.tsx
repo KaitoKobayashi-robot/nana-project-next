a1f1e450321a033f86d99ac80adeaef284ffcb1d
kaito <kaito2003kobayashi@gmail.com>
2026-01-15 22:13:42 +0900

    💄 imageを使用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { NavButtons } from "./_components/Navigation";
 import Logo from "../../public/logo.svg";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <Logo width={150} height={150} />
+      <Image
+        src="/home_port.svg"
+        alt="Home Port Logo"
+        width={150}
+        height={150}
+      />
       <div className="mt-10">
         <NavButtons />
       </div>
