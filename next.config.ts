9fa3c92ff3154b56a7b35dc470824a6dfc2cf727
kaito <kaito2003kobayashi@gmail.com>
2026-01-15 20:42:56 +0900

    👷 turbopackを削除

--- next.config.ts
+++ next.config.ts
@@ -45,14 +45,14 @@ const nextConfig: NextConfig = {
 
     return config;
   },
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
-      },
-    }
-  },
+  // turbopack: {
+  //   rules: {
+  //     "*.svg": {
+  //       loaders: ["@svgr/webpack"],
+  //       as: "*.js",
+  //     },
+  //   }
+  // },
 };
 
 export default nextConfig;
